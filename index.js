fa3b231bb8dc9fae5c9148cbe7494fdc3ec8e107
divyasree-2 <divyasreedandu28@gmail.com>
2025-05-01 17:34:53 +0530

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app = express();
-app.use(express.json());
-
-app.get('/', (req, res) => res.send('API is working'));
-app.listen(3001, () => console.log('Server running on port 3001'));
